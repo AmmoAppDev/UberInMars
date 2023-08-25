6b5348fef1ec626a8b20c863f42f00e339238230
Justin Ammons <Justinammons77@gmail.com>
2023-08-25 16:29:46 -0500

    Initial Commit

---
+++ Uberr/UberrApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberrApp.swift
+//  Uberr
+//
+//  Created by Justin Ammons on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct UberrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
